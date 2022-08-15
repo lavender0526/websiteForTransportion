85ea089229a91c3d24cd49943ae0a8fda19207f5
xxphjm <s43926027@gmail.com>
2022-08-16 00:46:33 +0800

    ok

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 
 # 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
+@app.route("/callback", methods=['GET'])
 def callback():
    
     return 'OK'
