5204eddeaedbd94fc920cade68d58ba64a019dd0
xxphjm <s43926027@gmail.com>
2022-08-16 00:44:38 +0800

    ok

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,28 @@
+from flask import Flask, request, abort
 
+
+
+
+#======這裡是呼叫的檔案內容=====
+
+#======這裡是呼叫的檔案內容=====
 
 #======python的函數庫==========
-import tempfile, os
-import datetime
-import time
-from flask import Flask
+
+#======python的函數庫==========
+
+app = Flask(__name__)
 
 
-
-
-
-app = Flask(__name__,template_folder='templates')
 
 # 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['GET'])
+@app.route("/callback", methods=['POST'])
 def callback():
-   return 'ok'
+   
+    return 'OK'
 
 
 # 處理訊息
-
-        
-import os
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=8000,debug=True)
+
+    app.run(host='0.0.0.0', port=8000)
